a6d05c34fbc3137d58ae352695ca1e4064cf6543
eshafik <shafikul.islam@me-solshare.com>
2020-07-07 21:39:41 +0600

    Updates

---
+++ precache-manifest.96865b3a8faded507db170c634c6c39a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "669d582b12a290bbbdb3de1d4517a741",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "0ef919116815274078e0",
+    "url": "/covid-tracker/static/css/main.a5374cf6.chunk.css"
+  },
+  {
+    "revision": "f0ec75304006a3c53bcd",
+    "url": "/covid-tracker/static/js/2.bd76114e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.bd76114e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ef919116815274078e0",
+    "url": "/covid-tracker/static/js/main.f859107a.chunk.js"
+  },
+  {
+    "revision": "09234177c6d3d9096b2d",
+    "url": "/covid-tracker/static/js/runtime-main.ee74c17a.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-tracker/static/media/covid_image.d7265326.png"
+  },
+  {
+    "revision": "f4a78f6e69441983a5a5290151a1860a",
+    "url": "/covid-tracker/static/media/shafik.f4a78f6e.jpeg"
+  }
+]);
